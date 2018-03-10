dd27933022edfaf292f5f48cd85b707cfae17025
everdrone <giorgiotropiano@gmail.com>
2018-03-10 03:37:10 +0100

    added declarations to snd.h

--- snd.h
+++ snd.h
@@ -12,21 +12,63 @@
 namespace snd {
 
 template<typename fp_t>
-bool isDenormal(fp_t);
+bool isDenormal(fp_t x);
 
 template<typename fp_t>
-fp_t denormalCancel(fp_t);
+fp_t denormalCancel(fp_t x);
+
+/**
+ * interpolation
+ */
+
+template <typename fp_t> fp_t interp_linear(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_bSpline(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_bSpline_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_lagrange(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_lagrange_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_hermite(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_hermite_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_2oOscu(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_2oOscu_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_watteTriLin(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_watteTriLin_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_parabolic2x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_parabolic2x_z(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_2p3o2x(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_2p3o4x(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_2p3o8x(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_2p3o16x(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_2p3o32x(fp_t x, fp_t y0, fp_t y1);
+template <typename fp_t> fp_t interp_4p2o2x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p2o4x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p2o8x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p2o16x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p2o32x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p3o2x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p3o4x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p3o8x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p3o16x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p3o32x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p4o2x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p4o4x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p4o8x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p4o16x(fp_t x, fp_t y[4]);
+template <typename fp_t> fp_t interp_4p4o32x(fp_t x, fp_t y[4]);
+
+/**
+ * end interpolation
+ */
 
 template <class fp_t>
 class Sine {
  public:
-  Sine(fp_t);
+  Sine(fp_t sampleRate);
   ~Sine();
 
   fp_t tick();
 
-  void setFrequency(fp_t);
-  void setPhase(fp_t);
+  void setFrequency(fp_t frequency);
+  void setPhase(fp_t phase);
 
   fp_t SR, frequency;
 
