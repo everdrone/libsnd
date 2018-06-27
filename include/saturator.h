2f42891ea8f6d9ab9065a9c409b7fe82b6ec58aa
everdrone <giorgiotropiano@gmail.com>
2018-06-27 14:05:35 +0200

    Add saturator.cpp to build config

--- include/saturator.h
+++ include/saturator.h
@@ -1,11 +1,13 @@
 #ifndef SATURATOR_H_
 #define SATURATOR_H_
 
+#include "approx.h"
+
 namespace snd {
 
 template <typename fp_t>
 fp_t tanhSaturator(fp_t x, fp_t level) {
-  return level * (snd::tanh(x / level));
+  return level * snd::tanh<fp_t>(x / level);
 }
 
 }; // !snd
