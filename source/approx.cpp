a9c15ac55eb9d1e9ee5fd62a090f2e42cfa2fe17
everdrone <giorgiotropiano@gmail.com>
2018-06-27 13:36:32 +0200

    Add template implementations for approx.h in approx.cpp

--- source/approx.cpp
+++ source/approx.cpp
@@ -8,4 +8,13 @@ template double tanRatApprox0Pi2<double>(double x);
 template float sineApprox7odd<float>(float x);
 template double sineApprox7odd<double>(double x);
 
+template float sin(float x);
+template double sin(double x);
+
+template float cos(float x);
+template double cos(double x);
+
+template float tanh(float x);
+template double tanh(double x);
+
 }; // !snd
